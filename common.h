5230b886eeca8a6e5ec0c06bda0bacfe09839c21
girendrababu-07 <jubirexona@gmail.com>
2026-04-09 20:11:38 +0530

    common.h

---
+++ common.h
@@ -0,0 +1,7 @@
+#ifndef COMMON_H              // Prevent multiple inclusion of this header file
+#define COMMON_H              // Define macro COMMON_H
+
+/* Magic string to identify whether stegged or not */  // Comment explaining purpose
+#define MAGIC_STRING "#*"     // Define magic string used to identify stego image
+
+#endif                       // End of header guard
